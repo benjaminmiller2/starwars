485f46cbd4fb3cfb7cd39b3ec1ef0f14f516439f
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-05-05 13:20:11 -0500

    second push

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let bodyParser = require('body-parser');
 let path = require('path');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // set up app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true}));
